f04c97fa2886964ac937d5bc6d999553a4c609b0
davidrambo2009 <davidrambo2009@gmail.com>
2025-07-15 11:17:39 -0400

    Update garage.js TOKENS TYPE NUMBER V1

--- models/garage.js
+++ models/garage.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const garageSchema = new mongoose.Schema({
   userId: String,
   cars: [{ name: String, serial: Number }]
+  tokens: { type: Number, default: 0 }
 });
 
 // Check if the model is already compiled (prevents OverwriteModelError)
